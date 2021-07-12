b879ec9008aafbb114283d42581e8f23e2ae6764
ahmedkhan <silvercat10@gmail.com>
2021-07-12 15:34:22 +1000

    First commit.

---
+++ blogs/urls.py
@@ -0,0 +1,15 @@
+"""Defines URL patterns for blogs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'blogs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # Page for adding a new blog post
+    path('new_blog_post/', views.new_blog_post, name='new_blog_post'),
+    # Page for adding a blog post
+    path('edit_blog_post/<int:blog_post_id>/', views.edit_blog_post, name='edit_blog_post'),
+]
---
+++ blogs/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+
+from .models import BlogPost
+from .forms import BlogPostForm
+
+# Create your views here.
+
+def index(request):
+    """The home page for Blog."""
+    blogposts = BlogPost.objects.order_by('date_added')
+    context = {'blogposts': blogposts}
+    return render(request, 'blogs/index.html', context)
+
+def new_blog_post(request):
+    """Add a new blog post."""
+    if request.method != 'POST':
+        # No data submitted to create a blank form.
+        form = BlogPostForm()
+    else:
+        # POST data submitted; process data.
+        form = BlogPostForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('blogs:index')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'blogs/new_blog_post.html', context)
+    
+def edit_blog_post(request, blog_post_id):
+    """Edit an existing blog post."""
+    blog_post = BlogPost.objects.get(id=blog_post_id)
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = BlogPostForm(instance=blog_post)
+    else:
+        # POST data submitted; process data.
+        form = BlogPostForm(instance=blog_post, data=request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'blog_post': blog_post, 'form': form}
+    return render(request, 'blogs/edit_blog_post.html', context)
