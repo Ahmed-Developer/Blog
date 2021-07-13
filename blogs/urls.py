6b66c2249b81c0cc137ac36deaba1c7e87e7d675
ahmedkhan <silvercat10@gmail.com>
2021-07-13 10:25:34 +1000

    edited index.html and urls.py

--- blogs/urls.py
+++ blogs/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page for adding a new blog post
     path('new_blog_post/', views.new_blog_post, name='new_blog_post'),
-    # Page for adding a blog post
+    # Page for editing a blog post
     path('edit_blog_post/<int:blog_post_id>/', views.edit_blog_post, name='edit_blog_post'),
 ]
