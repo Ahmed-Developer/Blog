6677dfba9f23ccc50976ce846322bb59ec3c4ae0
ahmedkhan <silvercat10@gmail.com>
2021-07-20 12:47:33 +1000

    Completed coding for app.

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import BlogPost
 from .forms import BlogPostForm
@@ -11,6 +13,7 @@ def index(request):
     context = {'blogposts': blogposts}
     return render(request, 'blogs/index.html', context)
 
+@login_required
 def new_blog_post(request):
     """Add a new blog post."""
     if request.method != 'POST':
@@ -20,16 +23,21 @@ def new_blog_post(request):
         # POST data submitted; process data.
         form = BlogPostForm(data=request.POST)
         if form.is_valid():
-            form.save()
-        return redirect('blogs:index')
+            new_blog_post = form.save(commit=False)
+            new_blog_post.owner = request.user
+            new_blog_post.save()
+            return redirect('blogs:index')
 
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'blogs/new_blog_post.html', context)
-    
+
+@login_required    
 def edit_blog_post(request, blog_post_id):
     """Edit an existing blog post."""
     blog_post = BlogPost.objects.get(id=blog_post_id)
+    if blog_post.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
